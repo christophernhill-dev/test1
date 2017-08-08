1b5d854e64a61b1d5d01da564f686df6b5c51004
christophernhill <cnh@mit.edu>
2017-08-08 17:22:32 -0400

    Update set_container_hosts.sh

--- ci/travis/functions/set_container_hosts.sh
+++ ci/travis/functions/set_container_hosts.sh
@@ -4,11 +4,21 @@
 #
 TPREF="foo"
 
-# Get list of IP addresses for container names passed as argument
+# Set list of container names from command argument list
 clist=$*
+
+# Get list of IP addresses for container names passed as argument
 (
 for n in ${clist} ; do
  nn=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${n}`
  echo ${n} ${nn}
 done
 ) > ${TPREF}_001.txt
+
+# Get /etc/hosts for each containter in list
+(
+for n in ${clist} ; do
+ docker ${n} exec -t -i cat /etc/hosts | awk '{print "'${n}': "$0}'
+done
+) > ${TPREF}_002.txt
+
